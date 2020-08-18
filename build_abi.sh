fb001010e9def32066e837cd5c4143ab8173b7dd
Jordan Crouse <jcrouse@qucinc.com>
2020-08-18 15:56:29 -0600

    build_abi.sh: Remove unnecessary change to COMMON_OUT_DIR
    The ABI dump section of the script incorrectly changes the value of
    COMMON_OUT_DIR and loses the OUT_DIR_PREFIX. There doesn't appear to
    be any need to change it at this point in the script.
    
    Bug: 73298553
    Change-Id: Ife719c716c7a1a31b0a5e3e41e594eaa7ee739fb
    Signed-off-by: Jordan Crouse <jcrouse@quicinc.com>

--- build_abi.sh
+++ build_abi.sh
@@ -233,7 +233,6 @@ echo "========================================================"
 echo " Creating ABI dump"
 
 # create abi dump
-COMMON_OUT_DIR=$(readlink -m ${OUT_DIR:-${ROOT_DIR}/out/${BRANCH}})
 id=${ABI_OUT_TAG:-$(git -C $KERNEL_DIR describe --dirty --always)}
 abi_out_file=abi-${id}.xml
 ${ROOT_DIR}/build/abi/dump_abi                \
