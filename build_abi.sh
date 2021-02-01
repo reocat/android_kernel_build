914580bef75d9169107b325e8c07699ff6e8e1e9
Matthias Maennich <maennich@google.com>
2021-02-01 23:54:46 +0000

    WIP: make ABI analysis hermetic
     - use hermetic toolchain
     - only wipe vmlinux and modules (to preserve the hermetic toolchain)
     - use llvm-nm instead of nm
    
    Bug: 175681515
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: Ia4379cd014b2074f5a633f15df4de5092dfabf95

--- build_abi.sh
+++ build_abi.sh
@@ -137,8 +137,6 @@ if [[ -z "$OUT_DIR" ]]; then
     wipe_out_dir=1
 fi
 
-# TODO (b/175681515)
-export HERMETIC_TOOLCHAIN=0
 source "${ROOT_DIR}/build/_setup_env.sh"
 
 if [ -z "${KMI_SYMBOL_LIST}" ]; then
@@ -152,7 +150,7 @@ fi
 
 # Now actually do the wipe out as above.
 if [[ $wipe_out_dir -eq 1 ]]; then
-    rm -rf "${COMMON_OUT_DIR}"
+    find "${COMMON_OUT_DIR}" -name 'vmlinux' -o -name '*.ko' -print -delete
 fi
 
 # inject CONFIG_DEBUG_INFO=y
