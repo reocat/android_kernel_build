7ff3bd52ed621223294d9c1e8309286eeddb1b64
Giuliano Procida <gprocida@google.com>
2021-06-01 11:21:16 +0100

    ABI: post-process abidw output with abitidy --all
    This will reduce the size of the generated ABI XML files and reduce
    churn when it's updated. Warnings will also be generated about untyped
    symbols and unresolvable duplicate type definitions.
    
    Bug: 187831743
    Change-Id: Iee3b8176e2c41148069b56efdcd1ca2303a53f03
    Signed-off-by: Giuliano Procida <gprocida@google.com>

--- abi/abitool.py
+++ abi/abitool.py
@@ -15,9 +15,11 @@
 # limitations under the License.
 #
 
+import logging
+import os
 import re
 import subprocess
-import logging
+import tempfile
 
 log = logging.getLogger(__name__)
 
@@ -160,26 +162,38 @@ class Libabigail(AbiTool):
     """" Concrete AbiTool implementation for libabigail """
     def dump_kernel_abi(self, linux_tree, dump_path, symbol_list,
             vmlinux_path=None):
-        dump_abi_cmd = ['abidw',
-                        # omit various sources of indeterministic abidw output
-                        '--no-corpus-path',
-                        '--no-comp-dir-path',
-                        # use (more) stable type ids
-                        '--type-id-style',
-                        'hash',
-                        # the path containing vmlinux and *.ko
-                        '--linux-tree',
-                        linux_tree,
-                        '--out-file',
-                        dump_path]
+        _, temp_path = tempfile.mkstemp()
+        try:
+            dump_abi_cmd = ['abidw',
+                            # omit various sources of indeterministic abidw output
+                            '--no-corpus-path',
+                            '--no-comp-dir-path',
+                            # use (more) stable type ids
+                            '--type-id-style',
+                            'hash',
+                            # the path containing vmlinux and *.ko
+                            '--linux-tree',
+                            linux_tree,
+                            '--out-file',
+                            temp_path]
 
-        if vmlinux_path is not None:
-            dump_abi_cmd.extend(['--vmlinux', vmlinux_path])
+            if vmlinux_path is not None:
+                dump_abi_cmd.extend(['--vmlinux', vmlinux_path])
 
-        if symbol_list is not None:
-            dump_abi_cmd.extend(['--kmi-whitelist', symbol_list])
+            if symbol_list is not None:
+                dump_abi_cmd.extend(['--kmi-whitelist', symbol_list])
 
-        subprocess.check_call(dump_abi_cmd)
+            subprocess.check_call(dump_abi_cmd)
+
+            tidy_abi_command = ['abitidy',
+                                '--all',
+                                '--input', temp_path,
+                                '--output', dump_path]
+
+            subprocess.check_call(tidy_abi_command)
+
+        finally:
+            os.remove(temp_path)
 
     def diff_abi(self, old_dump, new_dump, diff_report, short_report,
                  symbol_list, full_report):
