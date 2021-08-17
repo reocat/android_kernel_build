c35e70e30ca20102c30fb272cbdbe896decf8255
Yifan Hong <elsk@google.com>
2021-08-17 11:38:55 -0700

    kleaf: run docs server
    `bazel run //build/kleaf:docs_server` runs a local HTTP server to show
    the docs.
    
    This relies on http://showdownjs.com to render markdown files in
    a browser.
    
    Bug: 193531794
    Test: `bazel run //build/kleaf:docs_server`
    
    Change-Id: I5a4972342f7fabdbf05945de20c8c7c9675d572b

---
+++ kleaf/docs/render.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function get(url, successFn, failFn) {
+    var request = new XMLHttpRequest()
+    request.open("GET", url, true);
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                successFn(request);
+            } else {
+                failFn(request);
+            }
+        }
+    }
+    request.send(null)
+}
+
+function showMardownFile(responseText, file) {
+    var converter = new showdown.Converter();
+    converter.setOption("customizedHeaderId", "true")
+    converter.setOption("ghCompatibleHeaderId", "true")
+    document.getElementById("div-file-contents").innerHTML = converter.makeHtml(responseText);
+
+    // Show file name
+    document.getElementById("h1-file-name").innerText = file;
+    // Generate table of contents
+    var h2Elements = document.getElementById("div-file-contents").getElementsByTagName("h2")
+    var sampleToc = document.getElementById("link-sample-toc")
+    var toc = document.getElementById("div-toc")
+    for (var i = 0; i < h2Elements.length; i++) {
+        var clone = sampleToc.cloneNode(true)
+        clone.href = "#" + h2Elements[i].id
+        clone.innerText = h2Elements[i].id
+        toc.appendChild(clone)
+        toc.innerHTML += "<br />"
+    }
+
+    document.getElementById("div-full-file-contents").hidden = false;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Show directory
+    get(new URL("directory.html.frag", document.location).href, function(request) {
+        document.getElementById("div-directory").innerHTML = request.responseText;
+    }, function(request) {
+        document.getElementById("div-directory").innerText = "Failed to get directory.html.frag"
+    });
+
+    const params = new URLSearchParams(window.location.search);
+    const file = params.get("file")
+    if (!file) {
+        return;
+    }
+    if (!file.endsWith(".md")) {
+        document.body.innerText = "Invalid param for file";
+        return;
+    }
+    get(new URL(file, document.location).href, function (request) {
+        showMardownFile(request.responseText, file);
+    }, function (request) {
+        document.body.innerText = request.status + " " + request.statusText
+    })
+});
