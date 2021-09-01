12037f005102bf199a0903dfbe8e79f1d3a1dd83
Steve Muckle <smuckle@google.com>
2021-09-01 23:24:21 +0000

    build test_mappings.zip
    Bug: 186664401
    Change-Id: I2483e2e29e3f9833eb37d5321f3f175f6daddae9

--- build.sh
+++ build.sh
@@ -878,6 +878,18 @@ if [[ -z "${SKIP_EXT_MODULES}" ]] && [[ -n "${EXT_MODULES}" ]]; then
 
 fi
 
+echo "========================================================"
+echo " Generating test_mappings.zip"
+TEST_MAPPING_FILES=${OUT_DIR}/test_mapping_files.txt
+rm -f ${TEST_MAPPING_FILES}
+find ${ROOT_DIR} -name TEST_MAPPING \
+  -not -path "${ROOT_DIR}/out/*" \
+  -not -path "${ROOT_DIR}/out_abi*" \
+  -not -path "${ROOT_DIR}/\.repo*" \
+  >> ${TEST_MAPPING_FILES}
+soong_zip -o ${OUT_DIR}/test_mappings.zip -C ${ROOT_DIR} -l ${TEST_MAPPING_FILES}
+FILES="${FILES} test_mappings.zip"
+
 if [ -n "${EXTRA_CMDS}" ]; then
   echo "========================================================"
   echo " Running extra build command(s):"
