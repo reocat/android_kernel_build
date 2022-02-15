04b86d396300aed1731292d31542654c1ecf911a
Yifan Hong <elsk@google.com>
2022-02-15 12:20:33 -0800

    kleaf: Produce STABLE_SCM_VERSION in stable-status.txt
    The value of STABLE_SCM_VERSION is the result of
    setlocalversion executed on .source_date_epoch_dir.
    
    This allows the final binary to depend on this variable.
    
    Test: builds
    Bug: 202077908
    Change-Id: Iedf92be00a49e57e5773a55e9705edc493874d3a

---
+++ kleaf/workspace_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+WORKING_DIR=build/kernel/kleaf/workspace_status_dir
+KERNEL_DIR=$(readlink -f .source_date_epoch_dir)
+
+if [[ ! -d $KERNEL_DIR ]]; then
+  exit
+fi
+
+RESULT=$(cd $WORKING_DIR && $KERNEL_DIR/scripts/setlocalversion $KERNEL_DIR)
+echo STABLE_SCM_VERSION $RESULT
+
