e6474a1dd98046ec3985d4e5f973796098662652
Yifan Hong <elsk@google.com>
2022-02-15 12:20:33 -0800

    kleaf: Produce STABLE_SCM_VERSION in stable-status.txt
    The value of STABLE_SCM_VERSION is the result of
    setlocalversion executed on .source_date_epoch_dir.
    
    This allows the final binary to depend on this variable.
    
    Test: builds
    Bug: 202077908
    Change-Id: Iedf92be00a49e57e5773a55e9705edc493874d3a

---
+++ kleaf/workspace_status.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script used as --workspace_status_command.
+# Must execute at the root of workspace.
+
+if [[ ! -f "WORKSPACE" ]]; then
+  echo "ERROR: workspace_status.sh must be executed at the root of Bazel workspace." >&2
+  exit 1
+fi
+
+KERNEL_DIR=$(readlink -f ".source_date_epoch_dir")
+
+# Find scripts/setlocalversion.
+# First, .source_date_epoch_dir might be a device kernel source tree, so
+# look for setlocalversion there.
+# If that does not exist, look for setlocalversion in the ACK tree.
+SETLOCALVERSION=""
+if [[ "$KERNEL_DIR" ]] && [[ -x "$KERNEL_DIR/scripts/setlocalversion" ]]; then
+  SETLOCALVERSION="$KERNEL_DIR/scripts/setlocalversion"
+elif [[ -x "common/scripts/setlocalversion" ]]; then
+  SETLOCALVERSION="common/scripts/setlocalversion"
+fi
+
+STABLE_SCMVERSION=
+if [[ "$SETLOCALVERSION" ]] && [[ "$KERNEL_DIR" ]] && [[ -d "$KERNEL_DIR" ]]; then
+  WORKING_DIR=build/kernel/kleaf/workspace_status_dir
+  STABLE_SCMVERSION=$(cd "$WORKING_DIR" && "$SETLOCALVERSION" "$KERNEL_DIR")
+fi
+
+echo "STABLE_SCMVERSION $STABLE_SCMVERSION"
