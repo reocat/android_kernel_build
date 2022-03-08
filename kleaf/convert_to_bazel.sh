dd56dd3840e277fbc73a22d1bf9a17f9a7bd7b3e
Yifan Hong <elsk@google.com>
2022-03-08 10:39:11 -0800

    kleaf: Initial check-in of script to infer bazel command.
    This is the initial check-in of the script convert_to_bazel.sh,
    that infers the corresponding bazel command for a given build.sh
    command.
    
    Right now, the script finds all _kernel_build targets that
    corresponds to the build config same as _setup_env.sh:
    
      BUILD_CONFIG=${BUILD_CONFIG:-build.config}
    
    ... which uses the BUILD_CONFIG env, or the build.config file
    (symlink) at the top of repo.
    
    Test: manual
    Bug: 223403673
    
    Change-Id: I784f4478030de737e9d86e26e3f8dadcaf54b0d7

---
+++ kleaf/convert_to_bazel.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+ROOT_DIR=$($(dirname $(dirname $(readlink -m "$0")))/gettop.sh)
+
+targets=$(
+
+    cd $ROOT_DIR
+
+    # Same as _setup_env.sh. In theory we can also source _setup_env.sh, but to
+    # reduce overhead, just use this simple logic directly.
+    BUILD_CONFIG=${BUILD_CONFIG:-build.config}
+
+    if [[ ! -f $BUILD_CONFIG ]]; then
+        echo "ERROR: BUILD_CONFIG does not exist." >&2
+        exit 1
+    fi
+
+    # Assume that the build config is directly under a package
+    package_path=$(
+        cur_path=$(dirname $BUILD_CONFIG)
+        while [[ $cur_path != "." ]] && [[ ! -f $cur_path/BUILD.bazel ]] && [[ ! -f $cur_path/BUILD ]]; do
+            cur_path=$(dirname $cur_path)
+        done
+        echo $cur_path
+    )
+    if [[ $package_path == "." ]]; then
+        cat >&2 <<EOF
+    WARNING: Unable to determine package of build.config. Please migrate to Bazel.
+    See
+        https://android.googlesource.com/kernel/build/+/refs/heads/master/kleaf/README.md
+EOF
+        exit 1
+    fi
+    build_config_base=${BUILD_CONFIG#$package_path/}
+
+    package="//$package_path"
+    build_config_label="$package:$build_config_base"
+
+    script=$(cat <<EOF
+        let pkg_targets = set($package:*) in
+        let build_config_rdeps = attr(build_config, "$build_config_label", \$pkg_targets) in
+        let env = kind(_kernel_env, \$build_config_rdeps) in
+        let env_rdeps = same_pkg_direct_rdeps(\$env) in
+        let configs = kind(_kernel_config, \$env_rdeps) in
+        let configs_rdeps = same_pkg_direct_rdeps(\$configs) in
+        kind(_kernel_build, \$configs_rdeps)
+EOF
+    )
+
+    bazel query "$script" 2>/dev/null
+
+) # targets
+
+
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+# Intentionally not quote $targets so lines becomes tokens
+echo "bazel build" $targets
