6af31630873d1c28f8c03a2ac52d75afbc983b92
Yifan Hong <elsk@google.com>
2022-03-21 18:03:28 -0700

    kleaf: convert workspace_status.sh to Python
    Converting the script to Python allows us to execute multiple
    commands then collect the results at the end together (via
    Popen and communicate()). This allows parellel execution of
    shell commands that are easier to use than wait(1p).
    
    This change alone increases workspace_status.sh from
    0.7s to 0.9s. However, in the next CL,
    the time to execute workspace_status.sh is 0.9s compared to
    2.3s with a solution implemented in shell without parellel execution.
    
    Change-Id: I6625a81245e61b445a203cb44d778c1426cadcf3

---
+++ kleaf/workspace_status.py
@@ -0,0 +1,41 @@
+
+import os
+import subprocess
+
+import sys
+
+def collect(popen_obj):
+  stdout, _ = popen_obj.communicate()
+  if popen_obj.returncode != 0:
+    sys.stderr.write("ERROR: return code is {}\n".format(popen_obj.returncode))
+    sys.exit(1)
+  return stdout.strip()
+
+def main():
+  working_dir = "build/kernel/kleaf/workspace_status_dir"
+  kernel_dir = os.path.realpath(".source_date_epoch_dir")
+
+  setlocalversion = os.path.join(kernel_dir, "scripts/setlocalversion")
+  if not os.access(setlocalversion, os.X_OK):
+    setlocalversion = None
+
+  stable_scmversion_obj = None
+  if setlocalversion and os.path.isdir(kernel_dir):
+    stable_scmversion_obj = subprocess.Popen([setlocalversion, kernel_dir], text=True, stdout = subprocess.PIPE, cwd=working_dir)
+
+  stable_source_date_epoch = None
+  stable_source_date_epoch_obj = None
+  if os.environ.get("SOURCE_DATE_EPOCH"):
+    stable_source_date_epoch = os.environ["SOURCE_DATE_EPOCH"]
+  elif os.path.isdir(kernel_dir):
+    stable_source_date_epoch_obj = subprocess.Popen(["git", "-C", kernel_dir, "log", "-1", "--pretty=%ct"], text=True, stdout = subprocess.PIPE, cwd=working_dir)
+  else:
+    stable_source_date_epoch = 0
+
+  print("STABLE_SCMVERSION", collect(stable_scmversion_obj))
+  if stable_source_date_epoch_obj:
+    stable_source_date_epoch = collect(stable_source_date_epoch_obj)
+  print("STABLE_SOURCE_DATE_EPOCH", stable_source_date_epoch)
+
+if __name__ == '__main__':
+    main()
