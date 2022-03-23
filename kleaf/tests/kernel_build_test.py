e061ba4bd1d09b0686681d641bf2067e2eb5559d
Yifan Hong <elsk@google.com>
2022-03-22 17:03:58 -0700

    kleaf: Add kernel_build_test.
    Test that vmlinux has scmversion found when running 'strings'.
    
    Test: bazel test //common:kernel_aarch64_test
    Bug: 202077908
    
    Change-Id: I1bdc736850ebaaea8da60f86b81a0c1db9d40d58
    Signed-off-by: Yifan Hong <elsk@google.com>

---
+++ kleaf/tests/kernel_build_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os.path
+import re
+import shlex
+import subprocess
+
+import unittest
+import argparse
+
+
+def load_arguments():
+  # Path is filled by template expansion.
+  with open("{arguments_file}") as f:
+    content = f.read()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("artifacts", nargs="*")
+  return parser.parse_args(shlex.split(content))
+
+
+arguments = load_arguments()
+
+
+class ScmVersionTestCase(unittest.TestCase):
+  _scmversion_pattern = re.compile(
+    r"Linux version [0-9]+[.][0-9]+[.][0-9]+(-[0-9]+)?-g[0-9a-f]{6,40}")
+
+  def test_vmlinux_contains_scmversion(self):
+    """Test that vmlinux (if exists) has scmversion."""
+    for artifact in arguments.artifacts:
+      if os.path.basename(artifact) == "vmlinux":
+        strings = subprocess.check_output(["strings", artifact],
+                                          text=True).strip().splitlines()
+        self.assertTrue(any(
+            re.search(ScmVersionTestCase._scmversion_pattern, s) for s in
+            strings),
+                        "scmversion not found for vmlinux")
+
+
+if __name__ == '__main__':
+  unittest.main()
