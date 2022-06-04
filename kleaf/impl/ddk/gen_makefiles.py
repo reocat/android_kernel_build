278d50540034c9bb8a0ea661d06214737c972a5e
Yifan Hong <elsk@google.com>
2022-06-03 17:29:20 -0700

    kleaf: Initial commit of ddk_module
    This allows us to generate a Makefile for simple external
    kernel modules.
    
    Test: on raviole NFC module
    Bug: 210064480
    Change-Id: I81b470b5ded03843487a06327bdf2df33dd79dff

---
+++ kleaf/impl/ddk/gen_makefiles.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Generate a DDK module Makefile
+"""
+import argparse
+import os
+import shlex
+import textwrap
+import pathlib
+
+
+def _gen_makefile(package: str,
+    module_symvers_list: list[pathlib.Path],
+    output_makefiles: pathlib.Path,
+):
+  # kernel_module always executes in a sandbox. So ../ only traverses within the sandbox.
+  rel_root = "/".join([".."] * len(package.split("/")))
+
+  content = ""
+
+  for module_symvers in module_symvers_list:
+    content += textwrap.dedent(f"""\
+      EXTRA_SYMBOLS += $(OUT_DIR)/$(M)/{rel_root}/{module_symvers}
+      """)
+
+  content += textwrap.dedent("""\
+    modules modules_install clean:
+    \t$(MAKE) -C $(KERNEL_SRC) M=$(M) $(KBUILD_OPTIONS) KBUILD_EXTRA_SYMBOLS="$(EXTRA_SYMBOLS)" $(@)
+    """)
+
+  os.makedirs(output_makefiles, exist_ok=True)
+  with open(output_makefiles / "Makefile", "w") as out_file:
+    out_file.write(content)
+
+
+def gen_ddk_makefile(
+    output_makefiles: pathlib.Path,
+    kernel_module_out: pathlib.Path,
+    kernel_module_srcs: list[str],
+    include_dirs: list[str],
+    module_symvers_list: list[pathlib.Path],
+    package: str,
+):
+  package = os.path.normpath(package)
+
+  _gen_makefile(
+      package=package,
+      module_symvers_list=module_symvers_list,
+      output_makefiles=output_makefiles,
+  )
+
+  srcs = [pathlib.Path(src[len(package) + 1:]) for src in kernel_module_srcs if
+          os.path.commonpath((package, src)) == package]
+
+  assert kernel_module_out.suffix == ".ko", f"Invalid output: {kernel_module_out}"
+
+  kbuild = output_makefiles / kernel_module_out.parent / "Kbuild"
+  os.makedirs(kbuild.parent, exist_ok=True)
+
+  with open(kbuild, "w") as out_file:
+    out_file.write(f"obj-m += {kernel_module_out.with_suffix('.o').name}\n")
+    for src_str in srcs:
+      src = pathlib.Path(src_str)
+      # Ignore non-sources
+      if src.suffix != ".c":
+        continue
+      # Ignore self (don't omit obj-foo += foo.o)
+      if src.with_suffix(".ko") == kernel_module_out:
+        continue
+      assert src.parent == kernel_module_out.parent, \
+        f"{src} is not a valid source because it is not under {kernel_module_out.parent}"
+      out = src.with_suffix(".o").name
+      out_file.write(f"{kernel_module_out.with_suffix('').name}-y += {out}\n")
+
+    #    //path/to/package:target/name/foo.ko
+    # =>   path/to/package/target/name
+    rel_root_reversed = pathlib.Path(package) / kernel_module_out.parent
+    rel_root = "/".join([".."] * len(rel_root_reversed.parts))
+
+    out_file.write("\nccflags-y += \\\n")
+    for include_dir in include_dirs:
+      out_file.write("  ")
+      out_file.write(
+        shlex.quote(f"-I$(srctree)/$(src)/{rel_root}/{include_dir}"))
+      out_file.write("\\\n")
+    out_file.write("\n")
+
+  top_kbuild = output_makefiles / "Kbuild"
+  if top_kbuild != kbuild:
+    os.makedirs(output_makefiles, exist_ok=True)
+    with open(top_kbuild, "w") as out_file:
+      out_file.write(f"obj-y += {kernel_module_out.parent}/")
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument("--params", type=argparse.FileType())
+  parser.add_argument("--package")
+  parser.add_argument("--kernel-module-out", type=pathlib.Path)
+  parser.add_argument("--kernel-module-srcs", type=str, nargs="*")
+  parser.add_argument("--output-makefiles", type=pathlib.Path)
+  parser.add_argument("--include_dirs", type=str, nargs="*")
+  parser.add_argument("--module_symvers_list", type=pathlib.Path, nargs="*")
+
+  args = parser.parse_args()
+  if args.params:
+    args = parser.parse_args(args.params.read().splitlines())
+
+  delattr(args, "params")
+
+  gen_ddk_makefile(**vars(args))
