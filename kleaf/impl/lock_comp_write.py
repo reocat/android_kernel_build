c2297c171a445384130e02895e4bc292ff5eb48a
Yifan Hong <elsk@google.com>
2022-12-02 14:14:34 -0800

    kleaf: Detect hash collision in out/cache.
    We use Java's String.hashCode() to calculate
    the hash under out/cache, but it is prone to
    collision. Hence, detect it using flock().
    
    Test: manual
    Bug: 260955234
    Change-Id: I4ed82b441f0ec0b8197e4cb832b093eb5617bb3c

---
+++ kleaf/impl/lock_comp_write.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Lock a file, compare with stdin, then write stdin to the file if the file is
+empty or does not exist.
+
+Usage:
+  lock_comp_write <lock_file> < <in_file>
+
+The script does the following:
+- Put a lock on lock_file, creating it if it does not exist
+- If the file is empty, copy the content of stdin into lock_file.
+- Otherwise, compare stdin with lock_file, and exit abnormally if the content
+  mismatches.
+"""
+
+import errno
+import fcntl
+import os
+import sys
+import textwrap
+
+
+def die(msg):
+    sys.stderr.write(textwrap.dedent(f"""\
+        ERROR: Collision in {sys.argv[1]} detected! ({msg})
+            Run `tools/bazel clean` then try building again.
+            If the error persists, report a bug.
+        """))
+    sys.exit(1)
+
+
+class LockedFile(object):
+    def __init__(self, path: str):
+        self._path = path
+
+    def __enter__(self):
+        self._fd = os.open(self._path, os.O_RDWR | os.O_CREAT)
+        try:
+            fcntl.flock(self._fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except OSError as e:
+            if e.errno == errno.EACCES or e.errno == errno.EAGAIN:
+                die("File is already locked")
+            die(str(e))
+        return self._fd
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        fcntl.flock(self._fd, fcntl.LOCK_UN)
+        os.close(self._fd)
+        self._fd = None
+
+
+def copy_content(in_fd: int, out_fd: int):
+    """Copies the content of in_fd into out_fd."""
+    while True:
+        buf = os.read(in_fd, 4096)
+        if not buf:
+            break
+        os.write(out_fd, buf)
+
+
+def comp_file(in_fd: int, out_fd: int) -> bool:
+    """Compares the content of two file descriptors."""
+    while True:
+        in_buf = os.read(in_fd, 4096)
+        out_buf = os.read(out_fd, 4096)
+
+        if in_buf != out_buf:
+            return False
+
+        if not in_buf:
+            return True
+
+
+def comp_write(in_fd: int, out_fd: int) -> bool:
+    """Returns True iff in_fd has the same content of out_fd, or out_fd is empty.
+
+    If out_fd is empty, write in_fd into out_fd.
+    """
+    out_file_has_content = os.pread(out_fd, 1, 0)
+
+    if not out_file_has_content:
+        copy_content(in_fd, out_fd)
+        return True
+
+    return comp_file(in_fd, out_fd)
+
+
+with LockedFile(sys.argv[1]) as locked_fd:
+    out_content = os.pread(locked_fd, 4096, 0)
+
+    if not comp_write(sys.stdin.fileno(), locked_fd):
+        die("File content changes")
