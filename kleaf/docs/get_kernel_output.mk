42a50be58ba5dafc618c79c720ef888f053cdab8
Lecopzer Chen <lecopzer.chen@mediatek.com>
2022-12-12 22:17:43 +0800

    kleaf: Add support to get copy of OUT_DIR unconditionally
    The `OUT_DIR` is hided inside kleaf, unlike build.sh, we cannot
    get the path of `OUT_DIR`
    before build system starting. Unfortunately, some of automatically debugging and
    analyzing tools which need static `OUT_DIR` path would be broken.
    
    `--get_kernel_output` is for compatibility with Linux build and build.sh build
    which get `O`, a.k.a. `OUT_DIR` in kleaf, easily and get everything unconditionally.
    
    This is only for debugging or analyzing which would NOT affect any sandbox or caching
    mechanism (e.g. config=local) in Bazel.
    
    When the flag `--get_kernel_output=/target/path` is set, the `OUT_DIR` would be rsynced
    to `/target/path` directory.
    
    `/target/path` should be absolutely path only.
    
    Bug: 261145969
    
    Change-Id: Ic5d6a2137c6d932b804274231b46cdee0c440459
    Signed-off-by: Lecopzer Chen <lecopzer.chen@mediatek.com>

---
+++ kleaf/docs/get_kernel_output.mk
@@ -0,0 +1,26 @@
+# `get_kernel_output`
+
+The `OUT_DIR` is hided inside kleaf, unlike build.sh, we cannot get the path of `OUT_DIR`
+before build system starting. Unfortunately, some of automatically debugging and
+analyzing  tools which need static `OUT_DIR` path would be broken.
+
+`--get_kernel_output` is for compatibility with Linux build and build.sh build
+which get `O`, a.k.a `OUT_DIR` in kleaf, easily and get everything unconditionally.
+
+This is only for debugging or analyzing which would NOT affect any sandbox or caching
+mechanism (e.g. config=local) in Bazel.
+
+When the flag `--get_kernel_output=/target/path` is set, the `OUT_DIR` would be rsynced
+to `/target/path` directory.
+
+`/target/path` should be absolutely path only.
+
+
+For example:
+
+```shell
+$ bazel build --get_kernel_output=/target/path //common:kernel_aarch64
+```
+
+You will find all the things inside `OUT_DIR` are rsynced to the `/target/path`
+where `/target/path` is the destination with absolute path.
