ea095a6732186265695e23b56525c6426dd55313
Ramji Jiyani <ramjiyani@google.com>
2023-02-16 00:12:42 +0000

    ABI: Add script to verify trimmed ksymtab
    The utilization of abi/compare_to_symbol_list is to be substituted with
    abi/verify_ksymtab.py. The former performs two functions.
    
    Firstly, it ensures that all symbols specified in the vendor symbol list
    are exported in ksymtab, which is mandatory for enforcing KMI strict
    mode. The latter, i.e., verify_ksymtab.py, replaces this primary
    functionality.
    
    Secondly, it verifies that all symbols used by the vendor modules are
    listed in the vendor symbol lists to avoid trimming. This is imperative
    to ensure that the symbol list is accurate and prevent runtime module
    loading issues. If there is no symbol list, the runtime check does not
    enforce this verification and assumes that there is no ABI in place to
    support targets like x86_64. This check is being replaced by
    aosp/2447222.
    
    Bug: 269346251
    Test: http://b/269346251#comment10
    Test: TH
    Change-Id: I5c9dcb39e33211ec8d221ba55234ac813de3c6e6
    Signed-off-by: Ramji Jiyani <ramjiyani@google.com>

---
+++ abi/verify_ksymtab.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Verify every symbol in symbol list is exported in ksymtab.
+
+Usage:
+
+   KMI_STRICT_MODE_OBJECTS: Environement variable with objects whose
+                            exports should be consider to generate
+                            ksymtab for verification against KMI symbol
+                            list defaults to vmlinux.
+   verify_ksymtab.py [-h] --raw-kmi-symbol-list RAW_KMI_SYMBOL_LIST
+                          --symvers-file SYMVERS_FILE
+"""
+
+import argparse
+import os
+import sys
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "--raw-kmi-symbol-list",
+      required=True,
+      help="Symbol list",
+  )
+
+  parser.add_argument(
+      "--symvers-file",
+      required=True,
+      help="Module.symvers or vmlinux.symvers file to check ksymtab",
+  )
+
+  args = parser.parse_args()
+
+  # Objects whose exports to consider when building ksymtab for verification
+  objects = os.environ.get("KMI_STRICT_MODE_OBJECTS", "vmlinux").split()
+
+  # Parse Module.symvers, and ignore non-exported and vendor-specific symbols
+  ksymtab_symbols = []
+  with open(args.symvers_file) as symvers_file:
+    for line in symvers_file:
+      ksym = line.strip().split("\t")
+      # 1=symbol name; 2=object name; 3=export type
+      if ksym[3].startswith("EXPORT_SYMBOL") and ksym[2] in objects:
+        ksymtab_symbols.append(ksym[1])
+
+  # List of symbols defined in the raw_kmi_symbol_list
+  with open(args.raw_kmi_symbol_list) as raw_kmi_symbol_list_file:
+    kmi_symbols = raw_kmi_symbol_list_file.read().splitlines()
+
+  # Set difference to get elements in symbol list but not in ksymtab
+  missing_ksymtab_symbols = set(kmi_symbols) - set(ksymtab_symbols)
+  if missing_ksymtab_symbols:
+    print("Symbols missing from the ksymtab:")
+    for symbol in sorted(missing_ksymtab_symbols):
+      print(f"  {symbol}")
+    return 1
+
+  return 0
+
+
+if __name__ == "__main__":
+  sys.exit(main())
