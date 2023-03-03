f40f0240be5f8425d164e6bcee647f6b0bea5bd3
Yifan Hong <elsk@google.com>
2023-03-02 19:47:59 -0800

    gen_ddk_headers: fuzz
    Change-Id: I19e5a479eb8ec958372b1e1cbd1d0a76edd27325

--- kleaf/impl/ddk/gen_ddk_headers.py
+++ kleaf/impl/ddk/gen_ddk_headers.py
@@ -97,6 +97,15 @@ class Numfiles(object):
         return sum([len(files) for _, _, files in os.walk(self._path)])
 
 
+@dataclasses.dataclass(frozen=True)
+class FuzzySearchOneResult(object):
+    raw_include: pathlib.Path
+    files: Optional[Paths]
+
+    def __bool__(self):
+        return bool(self.files)
+
+
 @dataclasses.dataclass()
 class FuzzySearchResult(object):
     additional_files: PathsWithSourcesType = dataclasses.field(default_factory=PathsWithSources)
@@ -190,6 +199,10 @@ class GenDdkHeaders(buildozer_command_builder.BuildozerCommandBuilder):
 
         self._dump_debug("2_calc/package_includes.json", jsonify(self._package_includes))
 
+        # TODO let's think about fuzz later.
+        if self.args.fuzz:
+            self._fuzzy_resolve()
+
     def _handle_unresolved(self):
         # Can't find a good way to handle these yet, so let's output an
         # error message and asks for manual intervention.
@@ -212,6 +225,24 @@ class GenDdkHeaders(buildozer_command_builder.BuildozerCommandBuilder):
             self._generate_target(package, "all_headers_unsafe", files,
                                   self._package_includes[package],
                                   lambda x: not self._is_allowed(x))
+
+        if self.args.fuzz:
+            # Categorize files / includes by package
+            additional_by_package = collections.defaultdict(FuzzySearchResult)
+            for file, sources in self._fuzzy_resolve_result.additional_files.items():
+                additional_by_package[self._get_package(file)].additional_files[file] |= sources
+            for include, sources in self._fuzzy_resolve_result.additional_includes.items():
+                additional_by_package[self._get_package(include)].additional_includes[
+                    include] |= sources
+
+            for package, result in additional_by_package.items():
+                if not package:
+                    return
+                # TODO: Remove items in all_headers (allowlist) from graylist
+                self._generate_target(package, "all_headers_graylist",
+                                      result.additional_files.keys(),
+                                      result.additional_includes.keys(),
+                                      lambda x: True)
 
     def _get_package(self, directory: pathlib.Path) -> Optional[pathlib.Path]:
         dir_parts = directory.parts
@@ -301,6 +332,154 @@ class GenDdkHeaders(buildozer_command_builder.BuildozerCommandBuilder):
 
         return ret
 
+    def _fuzzy_resolve(self):
+        # List of all headers in known packages, relative to workspace root
+        all_headers = set(
+            pathlib.Path(root, file).relative_to(self._workspace_root())
+            for package in self.args.package
+            for root, _, files in os.walk(self._workspace_root() / package)
+            for file in files
+            if file.endswith(".h"))
+
+        self._dump_debug("3_fuzz/all_headers.json", jsonify(all_headers))
+
+        # key is package
+        self._fuzzy_resolve_result = FuzzySearchResult()
+
+        for package, files in self._package_files.items():
+            files_to_analyze = set(files)
+            all_files = set(self._sanitized_include_data.include_files.keys())
+            all_includes = set(self._sanitized_include_data.include_dirs.keys())
+
+            package_res = FuzzySearchResult()
+
+            pass_num = 0
+            while True:
+                # file -> #include directives. file is relative to workspace root.
+                # include_directives is relative to unknown directory that needs
+                # to be discovered later.
+                file_raw_includes: dict[pathlib.Path, Optional[Paths]] = {
+                    file: self._analyze_include_directives(file)
+                    for file in files_to_analyze
+                }
+
+                fuzzy_resolve_res: FuzzySearchResult = asyncio.run(
+                    self._fuzzy_resolve_package(all_headers, file_raw_includes,
+                                                all_files | package_res.additional_files.keys(),
+                                                all_includes | package_res.additional_includes.keys()))
+
+                if not fuzzy_resolve_res.additional_files and not fuzzy_resolve_res.additional_includes:
+                    break
+
+                # Sum
+                package_res |= fuzzy_resolve_res
+
+                # Prepare for next run
+                files_to_analyze = fuzzy_resolve_res.additional_files.keys()
+
+                pass_num += 1
+                logging.warning("%s: pass %d, found %d files, %d includes, %d unknowns",
+                                package,
+                                pass_num,
+                                len(fuzzy_resolve_res.additional_files),
+                                len(fuzzy_resolve_res.additional_includes),
+                                len(fuzzy_resolve_res.unknown_raw_include))
+
+                for key, value in vars(fuzzy_resolve_res).items():
+                    self._dump_debug(
+                        pathlib.Path("3_fuzz") / package / str(pass_num) / f"{key}.json",
+                        jsonify(value))
+
+            for key, value in vars(package_res).items():
+                self._dump_debug(pathlib.Path("3_fuzz") / package / f"{key}.json", jsonify(value))
+
+            self._fuzzy_resolve_result |= package_res
+
+        for key, value in vars(self._fuzzy_resolve_result).items():
+            self._dump_debug(pathlib.Path("3_fuzz") / f"{key}.json", jsonify(value))
+
+    async def _fuzzy_resolve_package(
+            self,
+            all_headers: Paths,
+            file_raw_includes: dict[pathlib.Path, Optional[list[pathlib.Path]]],
+            known_files: Paths,
+            known_include_dirs: Paths):
+        """Executes "fuzzy_resolve" for a package."""
+        ret_files = PathsWithSources()
+        ret_includes = PathsWithSources()
+        unknown = set()
+
+        known_files_reversed = type(self)._build_reverse_lookup_table(known_files)
+        all_headers_reversed = type(self)._build_reverse_lookup_table(all_headers)
+
+        files = list()
+        awaitables = list()
+        for file, raw_includes in file_raw_includes.items():
+            files.append(file)
+            awaitables.append(
+                self._fuzzy_resolve_parent(raw_includes, known_files_reversed,
+                                           all_headers_reversed))
+
+        all_results = await asyncio.gather(*awaitables)
+
+        for file, results in zip(files, all_results):
+            for result in results:
+                if result:
+                    found_matching = False
+                    for possible_child in result.files:
+                        possible_include_dir = prefix_of(possible_child, result.raw_include)
+                        if possible_include_dir in known_include_dirs:
+                            ret_files[possible_child].add(file)
+                            ret_includes[possible_include_dir].add(file)
+                            found_matching = True
+                            break
+                    if not found_matching:
+                        # Can't find the file in existing include directories, so this is a new
+                        # finding. Add all matched files.
+                        for child in result.files:
+                            ret_files[child].add(file)
+                            include_dir = prefix_of(child, result.raw_include)
+                            ret_includes[include_dir].add(file)
+                else:
+                    unknown.add(result.raw_include)
+
+        return FuzzySearchResult(
+            additional_files={k: v for k, v in ret_files.items() if k not in known_files},
+            additional_includes={k: v for k, v in ret_includes.items() if
+                                 k not in known_include_dirs},
+            unknown_raw_include=unknown,
+            known_files_reversed=known_files_reversed,
+            all_headers_reversed=all_headers_reversed,
+        )
+
+    async def _fuzzy_resolve_parent(self, raw_includes: Iterable[pathlib.Path],
+                                    known_headers_reversed: PathsWithSourcesType,
+                                    all_headers_reversed: PathsWithSourcesType):
+        """Executes "fuzzy_resolve" for a parent file that contains #include directives.."""
+        res = []
+        for raw_include in raw_includes:
+            res.append(
+                self._fuzzy_resolve_child(raw_include, known_headers_reversed,
+                                          all_headers_reversed))
+        return res
+
+    def _fuzzy_resolve_child(self, want: pathlib.Path,
+                             known_reversed: PathsWithSourcesType,
+                             fallback_reversed: PathsWithSourcesType) \
+            -> FuzzySearchOneResult:
+        """Executes "fuzzy_resolve" for a single #include directive."""
+        for lookup_table in (known_reversed, fallback_reversed):
+            if want in lookup_table:
+                return FuzzySearchOneResult(want, lookup_table[want])
+        return FuzzySearchOneResult(want, None)
+
+    def _is_excluded(self, item: pathlib.Path):
+        return any(re.search(pattern, str(item)) for pattern in self.args.exclude_regex)
+
+    def _is_allowed(self, item: pathlib.Path):
+        return any(item.is_relative_to(allowed) for allowed in self.args.allowed)
+
+
 def parse_args(argv: Sequence[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-i", "--input", help="Input directory or file from analyze_inputs",
@@ -344,6 +523,8 @@ def parse_args(argv: Sequence[str]) -> argparse.Namespace:
                         ],
                         help="""List of regex patterns that should not be added to the generated
                                 Bazel targets.""")
+    parser.add_argument("--fuzz", action="store_true",
+                        help="""Fuzzily search for transitive includes""")
     parser.add_argument("--dump", type=pathlib.Path,
                         help="""Directory that stores debug info.""")
     return parser.parse_args(argv)
