47c38bab3d97b0cc5606ff7d3178afc3b0d86ed7
Yifan Hong <elsk@google.com>
2023-08-08 15:09:45 -0700

    kleaf: checkpatch: add missing popd.
    Properly popd before exiting prematurely.
    
    In practice, this script is always run by Bazel in a subshell,
    so the change of directory by popd has no effect upon exiting.
    
    Test: TH
    Bug: 259995152
    Change-Id: I401b75038edeb16f1a8abdaa2b16db55960bfa68

--- kleaf/impl/checkpatch.sh
+++ kleaf/impl/checkpatch.sh
@@ -200,6 +200,7 @@ PATCH_FILE="${PATCH_DIR}/*.patch"
 
 if ! $(stat -t ${PATCH_FILE} >/dev/null 2>&1); then
   echo "Patch empty (probably due to suppressions). Skipping analysis."
+  popd
   exit 0
 fi
 
