fbe75fab814d7d56e6fa3db2493d9f8033b4ea27
Matthias Maennich <maennich@google.com>
2023-12-15 22:04:07 +0000

    Fix hermetic docker build
    Use Kleaf now that build.sh is gone.
    
    Change-Id: I5972697f52ed5518b3f533137afed0b80297a9a9
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- hermetic/run.sh
+++ hermetic/run.sh
@@ -16,10 +16,12 @@
 
 set -e
 
-BASE_DIR=$(readlink -f $(dirname $0)/../../)
+set -x
+
+BASE_DIR=$(readlink -f $(dirname $0)/../../../)
 
 # Build the hermetic container
-docker build -t hermetic $BASE_DIR/build/hermetic
+docker build -t hermetic $BASE_DIR/build/kernel/hermetic
 
 # Run the hermetic container
 docker run -ti                                                  \
