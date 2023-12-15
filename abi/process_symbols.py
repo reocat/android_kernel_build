b799f9a3b802a420a8ab8ea41bfb382fccf04524
Matthias Maennich <maennich@google.com>
2023-12-15 13:16:57 +0000

    Remove outdated references to build.sh in scripts and docs
    build.sh what?
    
    Change-Id: I4b5853c043d50cc104f45c4472d38191d505324e
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- abi/process_symbols.py
+++ abi/process_symbols.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# This is an internal helper used by build.sh and build_abi.sh
 
 import argparse
 import enum
