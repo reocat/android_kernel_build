4b0ab818c831da5b929cd612a4cf0399db673c51
Ulises Mendez Martinez <umendez@google.com>
2024-02-20 17:12:17 +0000

    Kleaf: checkpatch: Skip analyzis for rules_rust
    * This repository is problematic while doing `bazel query` due to some
      loads not using the prefix `@`.
    
    Bug: 326032978
    Change-Id: I0e79d810a4fa709a596a9af859002189b7af3093
    Signed-off-by: Ulises Mendez Martinez <umendez@google.com>

--- static_analysis/checkpatch_presubmit_bazel.py
+++ static_analysis/checkpatch_presubmit_bazel.py
@@ -40,6 +40,9 @@ _SILENT_ARGS = [
     "--noshow_progress",
 ]
 
+_DENY_LIST_PATH = [
+    pathlib.Path("external/bazelbuild-rules_rust"),
+]
 
 def load_arguments() -> dict[str, Any]:
     parser = argparse.ArgumentParser(
@@ -71,6 +74,10 @@ def _log_command(args):
 
 
 def _find_checkpatch_targets(path: pathlib.Path) -> list[str]:
+    if path in _DENY_LIST_PATH:
+      logging.info("Skipped //%s path in deny list", path)
+      return []
+
     if not _resolve_against_workspace_root(path / "BUILD.bazel").is_file() and \
         not _resolve_against_workspace_root(path / "BUILD").is_file():
         logging.info("//%s is not a package; no BUILD file is found", path)
