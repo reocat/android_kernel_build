e5150caa0d69065b7ed3f14e00e978a0e8b20525
Yifan Hong <elsk@google.com>
2024-03-22 17:05:03 -0700

    WIP: init_ddk download prebuilts
    Change-Id: I0c166b3ab07f8fe83b528abd0103f26bdb54565f

--- init/init_ddk.py
+++ init/init_ddk.py
@@ -16,13 +16,25 @@
 """Initializes the layout project needed to build DDK modules."""
 
 import argparse
+import concurrent.futures
+import dataclasses
+import io
+import json
 import logging
 import os
 import pathlib
+import shutil
+import ssl
+import subprocess
 import sys
+import tempfile
+from typing import Any, BinaryIO
+import urllib.parse
+import urllib.request
 
 _TOOLS_BAZEL = "tools/bazel"
 _MODULE_BAZEL_FILE = "MODULE.bazel"
+_ARTIFACT_URL_FMT = "https://androidbuildinternal.googleapis.com/android/internal/build/v3/builds/{build_id}/{build_target}/attempts/latest/artifacts/{filename}/url?redirect=true"
 
 _MODULE_BAZEL_CONTENT_TEMPLATE = """\
 \"""Kleaf: Build Android kernels with Bazel.\"""
@@ -33,17 +45,42 @@ local_path_override(
 )
 """
 
+_MODULE_BAZEL_PREBUILTS_CONTENT_TEMPLATE = """\
+kernel_prebuilt_ext = use_extension("@kleaf//build/kernel/kleaf:kernel_prebuilt_ext.bzl", "kernel_prebuilt_ext")
+kernel_prebuilt_ext.declare_kernel_prebuilts(
+    name = "gki_prebuilts",
+    local_artifact_path = "{prebuilts_dir}",
+    auto_download_config = True,
+)
+use_repo(kernel_prebuilt_ext,"gki_prebuilts")
+"""
+
 
 class KleafProjectSetterError(RuntimeError):
   pass
+
+
+ProjectMetadata = dict[str, Any]
+
+
+def _resolve(opt_path: pathlib.Path | None) -> pathlib.Path | None:
+  if opt_path:
+    return opt_path.resolve()
+  return None
 
 
 class KleafProjectSetter:
   """Initializes the layout project needed to build DDK modules."""
 
   def __init__(self, cmd_args: argparse.Namespace):
-    self.ddk_workspace: pathlib.Path | None = cmd_args.ddk_workspace
-    self.kleaf_repo_dir: pathlib.Path | None = cmd_args.kleaf_repo_dir
+    self.ddk_workspace = _resolve(cmd_args.ddk_workspace)
+    self.kleaf_repo_dir = _resolve(cmd_args.kleaf_repo_dir)
+    self.prebuilts_dir = _resolve(cmd_args.prebuilts_dir)
+    self.branch: str | None = cmd_args.branch
+    self.build_id: str | None = cmd_args.build_id
+    self.group: str = cmd_args.group
+    self.url_fmt: str = cmd_args.url_fmt
+    self.build_target: str = cmd_args.build_target
 
   def _symlink_tools_bazel(self):
     # TODO: b/328770706 -- Error handling.
@@ -62,15 +99,168 @@ class KleafProjectSetter:
       # TODO: b/328770706 -- Use markers to avoid overriding user overrides.
       f.write(
           _MODULE_BAZEL_CONTENT_TEMPLATE.format(
-              kleaf_repo_dir=self.kleaf_repo_dir
+              kleaf_repo_dir=self._try_rel_workspace(self.kleaf_repo_dir)
           )
       )
+      if self.prebuilts_dir:
+        f.write(
+            _MODULE_BAZEL_PREBUILTS_CONTENT_TEMPLATE.format(
+                prebuilts_dir=self._try_rel_workspace(self.prebuilts_dir)
+            )
+        )
+
+  def _try_rel_workspace(self, path: pathlib.Path):
+    """Tries to convert |path| to be relative to ddk_workspace."""
+    try:
+      return path.resolve().relative_to(self.ddk_workspace)
+    except ValueError:
+      return path
+
+  def _get_projects(self) -> dict[pathlib.Path, ProjectMetadata]:
+    assert self.build_info, "build_info is not set!"
+    project_list = self.build_info["parsed_manifest"]["projects"]
+    return {project_json["path"]: project_json for project_json in project_list}
+
+  def _project_in_group(self, project_metadata: ProjectMetadata) -> bool:
+    if self.group == "default" or self.group == "all":
+      return True
+    return self.group in project_metadata.get("groups", [])
+
+  def _kleaf_repo_dir_is_below_workspace(self):
+    try:
+      self.kleaf_repo_dir.relative_to(self.ddk_workspace)
+      return True
+    except ValueError:
+      return False
+
+  @staticmethod
+  def _git_init(path):
+    subprocess.check_call(["git", "init"], text=True, cwd=path)
+
+  @classmethod
+  def _is_git_directory(cls, path) -> bool:
+    return cls._get_git_root(path) is not None
+
+  @staticmethod
+  def _get_git_root(path) -> pathlib.Path | None:
+    try:
+      output = subprocess.check_output(
+          ["git", "rev-parse", "--show-toplevel"], text=True, cwd=path
+      )
+      return pathlib.Path(output.strip())
+    except subprocess.CalledProcessError:
+      return None
+
+  @staticmethod
+  def _add_submodules(git_root, kleaf_repo_dir, projects):
+    # TODO: b/328770706: Option to use Git or repo to sync
+    # raise NotImplementedError
+    pass
+
+  @staticmethod
+  def _checkout_projects(kleaf_rep_dir, projects):
+    # TODO: b/328770706: Option to use Git or repo to sync
+    # raise NotImplementedError
+    pass
+
+  def _set_build_info(self):
+    assert self.build_id, "build_id is not set!"
+    # TODO: b/328770706: This is only supported on ci.android.com
+    # TODO: Relying on build_info is fragile. We should create our own mechanism.
+    build_info_fp = io.BytesIO()
+    self._download("BUILD_INFO", build_info_fp)
+    build_info_fp.seek(0)
+    self.build_info = json.load(
+        io.TextIOWrapper(build_info_fp, encoding="utf-8")
+    )
+
+  def _infer_download_list(self) -> list[str]:
+    assert self.build_info, "build_info is not set!"
+    # TODO type checking
+    return self.build_info["target"]["dir_list"]
+
+  def _download(self, remote_filename, out_f: BinaryIO, close: bool = False):
+    url = self.url_fmt.format(
+        build_id=self.build_id,
+        build_target=self.build_target,
+        filename=urllib.parse.quote(remote_filename, safe=""),  # / -> %2F
+    )
+    try:
+      # FIXME: For demo purposes, do not verify cert. DO NOT SUBMIT THIS!
+      ctx = ssl.create_default_context()
+      ctx.check_hostname = False
+      ctx.verify_mode = ssl.CERT_NONE
+
+      with urllib.request.urlopen(url, context=ctx) as in_f:
+        print(f"Scheduling download for {remote_filename}")
+        if close:
+          with out_f:
+            shutil.copyfileobj(in_f, out_f)
+        else:
+          shutil.copyfileobj(in_f, out_f)
+    except urllib.error.URLError:
+      raise RuntimeError(f"Fail to download {url}")
+
+  def _checkout_source_tree(self):
+    assert self.build_id, "build_id is not set!"
+
+    projects = self._get_projects()
+
+    projects = {
+        project: project_metadata
+        for project, project_metadata in projects.items()
+        if self._project_in_group(project_metadata)
+    }
+
+    self.kleaf_repo_dir.mkdir(parents=True, exist_ok=True)
+    self.ddk_workspace.mkdir(parents=True, exist_ok=True)
+
+    if self._kleaf_repo_dir_is_below_workspace():
+      if not self._is_git_directory(self.ddk_workspace):
+        self._git_init(self.ddk_workspace)
+      git_root = self._get_git_root(self.ddk_workspace)
+      self._add_submodules(git_root, self.kleaf_repo_dir, projects)
+    else:
+      self._checkout_projects(self.kleaf_repo_dir, projects)
+
+  def _download_prebuilts(self):
+    assert self.build_id, "build_id is not set!"
+
+    if not self.prebuilts_dir:
+      return
+
+    # TODO: b/328770706: download less files
+    files = self._infer_download_list()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+      futures = []
+      for file in files:
+        dst = self.prebuilts_dir / file
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        futures.append(
+            executor.submit(self._download, file, open(dst, "wb"), close=True)
+        )
+      for complete_ret in concurrent.futures.as_completed(futures):
+        complete_ret.result()  # Raise exception if any
 
   def _handle_local_kleaf(self):
     self._symlink_tools_bazel()
     self._generate_module_bazel()
 
   def run(self):
+    if self.branch or self.build_id:
+      if not self.build_id:
+        pass
+        # TODO: b/328770706: Infer tip of branch build id
+        # self.build_id = ...
+      self._set_build_info()
+
+      if self.prebuilts_dir:
+        self._download_prebuilts()
+
+      if self.ddk_workspace and self.kleaf_repo_dir:
+        self._checkout_source_tree()
+
     if self.ddk_workspace and self.kleaf_repo_dir:
       self._handle_local_kleaf()
 
@@ -93,6 +283,12 @@ if __name__ == "__main__":
       default=None,
   )
   parser.add_argument(
+      "--prebuilts_dir",
+      help="Path to prebuilts",
+      type=pathlib.Path,
+      default=None,
+  )
+  parser.add_argument(
       "--branch",
       help="Android Kernel branch from CI.",
       type=pathlib.Path,
@@ -102,7 +298,7 @@ if __name__ == "__main__":
       "--url_fmt",
       help="URL format endpoint for CI downloads.",
       # TODO: b/328770706 -- Set default value.
-      default=None,
+      default=_ARTIFACT_URL_FMT,
   )
   parser.add_argument(
       "--build_id",
@@ -114,6 +310,15 @@ if __name__ == "__main__":
       type=str,
       help='the build target to download, e.g. "kernel_aarch64"',
       default="kernel_aarch64",
+  )
+  # TODO: make plural, just like repo init -g
+  parser.add_argument(
+      "--group",
+      help=(
+          "Group to check out. If `all`, check out all projects, including"
+          " kernel sources"
+      ),
+      default="ddk",
   )
   args = parser.parse_args()
   logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
