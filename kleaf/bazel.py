8288d95b775db1b8e82e17b862295400a7171928
Yifan Hong <elsk@google.com>
2024-04-30 13:37:45 -0700

    kleaf: wrapper handles return code when running as subprocess
    When the internal bazel binary is executed as a subprocess,
    the return code from process.wait() is never checked. Wrap
    it with a coroutine that actually checks the return code.
    
    Test: integration test
    Change-Id: Ifaa134658f823fab2dbf534092e84700fa9dc29c

--- kleaf/bazel.py
+++ kleaf/bazel.py
@@ -59,6 +59,13 @@ class BazelWrapperException(Exception):
         self.message = message
 
 
+class BazelSubprocessException(BazelWrapperException):
+    """The internal `bazel` call fails."""
+
+    def __init__(self, code: int):
+        BazelWrapperException.__init__(self, code=code)
+
+
 class MultipleBazelWrapperException(BazelWrapperException):
     def __init__(self, errors: list[BazelWrapperException]):
         """Wraps multiple BazelWrapperException into one.
@@ -631,7 +638,6 @@ class OutputMutator:
         self,
         input_stream: BinaryIO,
         output_stream: BinaryIO,
-        stream_name: str,
     ):
         """Pipes input to output, optionally mutating lines.
 
@@ -648,6 +654,13 @@ class OutputMutator:
                 output = output_decoded.encode()
             output_stream.buffer.write(output)
             output_stream.flush()
+
+
+async def _wait_for_subprocess(process):
+    """Wraps process.wait() and raises if exit code is non-zero."""
+    return_code = await process.wait()
+    if return_code != 0:
+        raise BazelSubprocessException(code=return_code)
 
 
 async def run(command, env, epilog_coroutine, output_mutator):
@@ -678,7 +691,7 @@ async def run(command, env, epilog_coroutine, output_mutator):
     coroutines = [
         stderr_coroutine,
         stdout_coroutine,
-        process.wait(),
+        _wait_for_subprocess(process),
     ]
     tasks = [asyncio.Task(coroutine) for coroutine in coroutines]
     done, _ = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
