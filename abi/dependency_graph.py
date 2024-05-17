e32fe1b3f94d0e1a7abe239ed275414b121e296b
Ulises Mendez Martinez <umendez@google.com>
2024-05-17 10:04:22 +0000

    [WIP] Kleaf: Create a dependency graph from symbols
    Bug: 297175619
    Change-Id: I5883a7fcd3fc538545fd018b0099ce2a4ecddd12
    Signed-off-by: Ulises Mendez Martinez <umendez@google.com>

---
+++ abi/dependency_graph.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Utility function to provide a best effort dependency graph.
+
+import argparse
+import hashlib
+import pathlib
+import sys
+
+import symbol_extraction
+
+
+def find_binaries(
+    directory: pathlib.Path,
+) -> (pathlib.Path | None, list[pathlib.Path]):
+    """Locates vmlinux and kernel modules (*.ko)."""
+    vmlinux = list(directory.glob("**/vmlinux"))
+    modules = list(directory.glob("**/*.ko"))
+    # TODO: Error out if multiple vmlinux are found.
+    if not vmlinux:
+        return None, modules
+    return vmlinux[0], modules
+
+
+def extract_exports_map(
+    blobs: list[pathlib.Path],
+) -> dict[pathlib.Path, list[str]]:
+    """Extracts the ksymtab exported symbols for a list of objects."""
+    return {
+        pathlib.Path(blob).name: symbol_extraction.extract_exported_symbols(
+            blob
+        )
+        for blob in blobs
+    }
+
+
+def extract_undefined_symbols_multiple(
+    modules: list[pathlib.Path],
+) -> dict[pathlib.Path, list[str]]:
+    """Extracts undefined symbols from a list of module files."""
+    return {
+        pathlib.Path(module).name: symbol_extraction.extract_undefined_symbols(
+            module
+        )
+        for module in modules
+    }
+
+
+def create_graphviz(
+    modules: list[str],
+    adjacency_list: list[set],
+    output: pathlib.Path,
+    colors: bool,
+):
+    "Creates a diagram to display a graph using dot syntax."
+    content = ["digraph {"]
+    content.extend([
+        "\tgraph [rankdir=LR, splines=ortho];",
+        "\tnode [color=steelblue, shape=plaintext];",
+        "\tedge [arrowhead=odot, color=olive];",
+    ])
+    for from_id, neighbors in enumerate(adjacency_list):
+        # vmlinux is dependency for most of the nodes so skip it.
+        if modules[from_id] == "vmlinux":
+            continue
+        # Skip nodes without dependants.
+        if not neighbors:
+            # print(f"Skipping leaf module {modules[from_id]}")
+            continue
+        edges = []
+        for neighbor in neighbors:
+            edges.append(f'"{modules[neighbor]}"')
+        edge_str = ",".join(edges)
+        # Customize edge colors.
+        edge_color = ""
+        if colors:
+            h = hashlib.shake_256(edge_str.encode())
+            edge_color = f' [color="  # {h.hexdigest(3)}"]'
+        content.append(f'\t"{modules[from_id]}" -> {edge_str}{edge_color};')
+    content.append("}")
+    out = pathlib.Path(output)
+    out.write_text("\n".join(content), encoding="utf-8")
+
+
+def process_graph(
+    symbols: dict[pathlib.Path, list[str]],
+    exports: dict[pathlib.Path, list[str]],
+    output: pathlib.Path,
+    colors: bool,
+):
+    "Creates a best effort dependency graph from symbols."
+    idx = dict()
+    symbol_to_module = dict()
+    adjacency_list = list()
+    modules = list()
+
+    # Map symbol exported to module which exposes.
+    for module, exported in exports.items():
+        if not module in idx:
+            idx[module] = len(idx)
+            adjacency_list.append(set())
+            modules.append(module.removesuffix(".ko"))
+        exporter = idx.get(module)
+        for symbol in exported:
+            symbol_to_module[symbol] = exporter
+
+    # Update the adjacency_list based on the links created by the undefined symbols.
+    for module, undefined in symbols.items():
+        to_id = idx.get(module)
+        for symbol in undefined:
+            if symbol not in symbol_to_module:
+                continue
+            from_id = symbol_to_module[symbol]
+            adjacency_list[from_id].add(to_id)
+
+    # Draw the dependencies.
+    create_graphviz(modules, adjacency_list, output, colors)
+
+
+def main():
+    """Extracts the required symbols for a directory full of kernel modules."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "directory",
+        type=pathlib.Path,
+        help="the directory to search for kernel binaries",
+    )
+    parser.add_argument("output", help="Where to store the output")
+    parser.add_argument(
+        "--colors",
+        action="store_true",
+        help=(
+            "Edges to dependents of a module share the same color. This is"
+            " useful to differentiate dependencies of a module."
+        ),
+    )
+
+    args = parser.parse_args()
+
+    if not args.directory.is_dir():
+        print(f"Expected a directory with binaries, but got {args.directory}")
+        return 1
+
+    # Locate the Kernel Binaries.
+    vmlinux, modules = find_binaries(args.directory)
+
+    # Extract undefined symbols and exported modules.
+    symbols = extract_undefined_symbols_multiple(modules)
+    exports = extract_exports_map([vmlinux] + modules)
+
+    # Create a dependency graph & visualization.
+    process_graph(symbols, exports, args.output, args.colors)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ kleaf/impl/abi/dependency_graph.bzl
@@ -0,0 +1,112 @@
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Extracts symbols from kernel binaries."""
+
+load(":abi/abi_transitions.bzl", "abi_common_attrs", "notrim_transition")
+load(
+    ":common_providers.bzl",
+    "KernelBuildAbiInfo",
+    "KernelModuleInfo",
+    "KernelSerializedEnvInfo",
+)
+load(":debug.bzl", "debug")
+load(":utils.bzl", "kernel_utils", "utils")
+
+visibility("//build/kernel/kleaf/...")
+
+def _dependency_graph_impl(ctx):
+    out = ctx.actions.declare_file("{}/dependency_graph.dot".format(ctx.attr.name))
+    intermediates_dir = utils.intermediates_dir(ctx)
+    vmlinux = utils.find_file(
+        name = "vmlinux",
+        files = ctx.files.kernel_build,
+        what = "{}: kernel_build".format(
+            ctx.attr.name,
+        ),
+        required = True,
+    )
+    in_tree_modules = utils.find_files(suffix = ".ko", files = ctx.files.kernel_build)
+    srcs = [vmlinux]
+    srcs += in_tree_modules
+
+    # external modules
+    for kernel_module in ctx.attr.kernel_modules:
+        if KernelModuleInfo in kernel_module:
+            srcs += kernel_module[KernelModuleInfo].files.to_list()
+        else:
+            srcs += kernel_module.files.to_list()
+
+    inputs = [] + srcs
+    transitive_inputs = [ctx.attr.kernel_build[KernelSerializedEnvInfo].inputs]
+    tools = [ctx.executable._dependency_graph]
+    transitive_tools = [ctx.attr.kernel_build[KernelSerializedEnvInfo].tools]
+
+    # Get the signed and stripped module archive for the GKI modules
+    base_modules_archive = ctx.attr.kernel_build[KernelBuildAbiInfo].base_modules_staging_archive
+    if not base_modules_archive:
+        base_modules_archive = ctx.attr.kernel_build[KernelBuildAbiInfo].modules_staging_archive
+    inputs.append(base_modules_archive)
+
+    command = kernel_utils.setup_serialized_env_cmd(
+        serialized_env_info = ctx.attr.kernel_build[KernelSerializedEnvInfo],
+        restore_out_dir_cmd = utils.get_check_sandbox_cmd(),
+    )
+    command += """
+        mkdir -p {intermediates_dir}
+        # Extract archive and copy the modules from the base kernel first.
+        mkdir -p {intermediates_dir}/temp
+        tar xf {base_modules_archive} -C {intermediates_dir}/temp
+        find {intermediates_dir}/temp -name '*.ko' -exec mv -t {intermediates_dir} {{}} \\;
+        rm -rf {intermediates_dir}/temp
+        # Copy other inputs including vendor modules;
+        cp -pfl {srcs} {intermediates_dir}
+        {dependency_graph} {intermediates_dir} {output}
+        rm -rf {intermediates_dir}
+    """.format(
+        srcs = " ".join([file.path for file in srcs]),
+        intermediates_dir = intermediates_dir,
+        dependency_graph = ctx.executable._dependency_graph.path,
+        output = out.path,
+        base_modules_archive = base_modules_archive.path,
+    )
+    debug.print_scripts(ctx, command)
+    ctx.actions.run_shell(
+        inputs = depset(inputs, transitive = transitive_inputs),
+        outputs = [out],
+        command = command,
+        tools = depset(tools, transitive = transitive_tools),
+        progress_message = "Obtaining dependency graph {}".format(ctx.label),
+        mnemonic = "KernelDependencyGraph",
+    )
+
+    return DefaultInfo(files = depset([out]))
+
+dependency_graph = rule(
+    implementation = _dependency_graph_impl,
+    attrs = {
+        "kernel_build": attr.label(providers = [KernelSerializedEnvInfo, KernelBuildAbiInfo]),
+        "kernel_modules": attr.label_list(allow_files = True),
+        "_dependency_graph": attr.label(
+            default = "//build/kernel:dependency_graph",
+            cfg = "exec",
+            executable = True,
+        ),
+        "_debug_print_scripts": attr.label(default = "//build/kernel/kleaf:debug_print_scripts"),
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    } | abi_common_attrs(),
+    cfg = notrim_transition,
+)
