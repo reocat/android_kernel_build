eb9c8b3e8bdba48572fe8a3e45d8a5b7d7889238
Aleksei Vetrov <vvvvvv@google.com>
2024-05-21 19:38:15 +0000

    Add ABI compliance script
    Add script to process ABI report from dist builds and fail if ABI
    differences was found.
    
    Bug: 341797800
    Change-Id: I9212eb8502c49bb92a5575ea77c2ec495ae7c082

---
+++ abi_compliance.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is used as a test after
+#  `tools/bazel //common:kernel_aarch64_abi_dist``
+# to check if the build reported ABI differences. It exits with non-zero error
+# code if ABI report file is missing or when it is not empty.
+
+# Example:
+#   tools/bazel //common:kernel_aarch64_abi_dist
+#   build/kernel/abi_compliance.sh out_abi/kernel_aarch64/dist/abi_stgdiff
+
+abi_reports_path="$1"
+abi_report=$(cat "${abi_reports_path}/abi.report.short")
+
+if [ -n "${abi_report}" ]; then
+    echo 'ERROR: ABI DIFFERENCES HAVE BEEN DETECTED!' >&2
+    echo "ERROR: ${abi_report}" >&2
+    exit 1
+fi
+
+echo 'INFO: no ABI differences reported by dependency target build.'
