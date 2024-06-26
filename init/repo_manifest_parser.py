4b34344804cbd8ffea2266f08fe9e0eedfa81f4a
HONG Yifan <elsk@google.com>
2024-06-26 15:34:08 -0700

    kleaf: init_ddk skip fixing paths for ddk-external projects.
    Do not fix up projects for ddk-external; they
    should stay below repo root directly.
    
    Bug: 291918721
    Change-Id: Ibb2a1a526b3cbbfc86982995a360c146f10a11ee

--- init/repo_manifest_parser.py
+++ init/repo_manifest_parser.py
@@ -24,6 +24,17 @@ from typing import TextIO
 from init.init_errors import KleafProjectSetterError
 
 
+@dataclasses.dataclass(frozen=True)
+class ProjectState:
+    # Paths are relative to repo root.
+
+    # Original path in the repo manifest in the build
+    original_path: pathlib.Path
+
+    # Fixed up path below the current repo root.
+    fixed_path: pathlib.Path
+
+
 @dataclasses.dataclass
 class RepoManifestParser:
     """Parses the repo manifest from a build."""
@@ -31,27 +42,31 @@ class RepoManifestParser:
     project_prefix: pathlib.Path
 
     # If None, add all projects. If a set, only add projects that matches
-    # any of these groups. If an empty set, no project is added.
-    groups: set[str] | None
+    # any of these groups. If both an empty set, no project is added.
+
+    # list of projects with paths fixed up below project_prefix
+    fixup_groups: set[str] | None
+    # list of projects with paths untouched
+    preserve_groups: set[str] | None
 
     def write_transformed_dom(self, file: TextIO) \
-            -> list[pathlib.Path]:
+            -> set[ProjectState]:
         """Transforms manifest from the build and write result to file.
 
         Returns:
-            list of Git project paths relative to repo root
+            set of ProjectState objects describing old and new paths.
         """
         try:
             with xml.dom.minidom.parseString(self.manifest) as dom:
-                project_paths = self._transform_dom(dom)
+                project_states = self._transform_dom(dom)
                 dom.writexml(file)
-                return project_paths
+                return project_states
         except xml.parsers.expat.ExpatError as err:
             raise KleafProjectSetterError("Unable to parse repo manifest") \
                 from err
 
     def _transform_dom(self, dom: xml.dom.minidom.Document) \
-            -> list[pathlib.Path]:
+            -> set[ProjectState]:
         """Transforms manifest from the build.
 
         - Append project_prefix to each project.
@@ -59,26 +74,35 @@ class RepoManifestParser:
         - Drop elements that may conflict with the main manifest
 
         Returns:
-            list of Git project paths relative to repo root
+            set of ProjectState objects describing old and new paths.
         """
         root: xml.dom.minidom.Element = dom.documentElement
         projects = root.getElementsByTagName("project")
         defaults = self._parse_repo_manifest_defaults(root)
-        project_paths = []
+        project_states = set()
         for project in projects:
-            if not self._match_group(project):
+            category = self._match_group(project)
+            if category == "delete":
                 root.removeChild(project).unlink()
                 continue
+
+            for key, value in defaults.items():
+                if not project.hasAttribute(key):
+                    project.setAttribute(key, value)
 
             # https://gerrit.googlesource.com/git-repo/+/master/docs/manifest-format.md#element-project
             orig_path_below_repo = pathlib.Path(project.getAttribute("path") or
                                                 project.getAttribute("name"))
+
+            if category == "preserve":
+                project_states.add(ProjectState(orig_path_below_repo,
+                                                orig_path_below_repo))
+                continue
+
             path_below_repo = self.project_prefix / orig_path_below_repo
-            project_paths.append(path_below_repo)
+            project_states.add(ProjectState(
+                orig_path_below_repo, path_below_repo))
             project.setAttribute("path", str(path_below_repo))
-            for key, value in defaults.items():
-                if not project.hasAttribute(key):
-                    project.setAttribute(key, value)
 
             for link in project.getElementsByTagName("linkfile"):
                 orig_dest = pathlib.Path(link.getAttribute("dest"))
@@ -90,14 +114,22 @@ class RepoManifestParser:
             root.removeChild(superproject).unlink()
         for default_element in root.getElementsByTagName("default"):
             root.removeChild(default_element).unlink()
-        return project_paths
+        return project_states
 
-    def _match_group(self, project: xml.dom.minidom.Element):
-        """Returns true if project matches any of groups."""
-        if self.groups is None:
+    def _match_group(self, project: xml.dom.minidom.Element) -> str:
+        """Returns category of the groups if project matches any of groups."""
+        # preserve_groups has higher priority.
+        if self._match_group_internal(project, self.preserve_groups):
+            return "preserve"
+        if self._match_group_internal(project, self.fixup_groups):
+            return "fixup"
+        return "delete"
+
+    def _match_group_internal(self, project: xml.dom.minidom.Element, expect_groups: set[str] | None):
+        if expect_groups is None:
             return True
         project_groups = re.split(r",| ", project.getAttribute("groups"))
-        return bool(set(project_groups) & self.groups)
+        return bool(set(project_groups) & expect_groups)
 
     def _parse_repo_manifest_defaults(self, root: xml.dom.minidom.Element):
         """Parses <default> in a repo manifest. """
