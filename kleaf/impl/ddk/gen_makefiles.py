5776ecf1ff792f99ec8363ef11764ece8837fbe4
Ulises Mendez Martinez <umendez@google.com>
2024-07-02 16:37:56 +0000

    Add built with DDK info
    Bug: 307910700
    Change-Id: I34e719242a15cc5c8e45565eb0872c6ab40a946a
    Signed-off-by: Ulises Mendez Martinez <umendez@google.com>

--- kleaf/impl/ddk/gen_makefiles.py
+++ kleaf/impl/ddk/gen_makefiles.py
@@ -68,16 +68,26 @@ def die(*args, **kwargs):
 
 
 def _get_license_str():
-  return textwrap.dedent("""\
-    # SPDX-License-Identifier: GPL-2.0
+    return textwrap.dedent("""\
+      # SPDX-License-Identifier: GPL-2.0
 
-  """)
+    """)
+
+def _get_ddk_marker():
+    return textwrap.dedent("""\
+      KBUILD_OPTIONS += CONFIG_BUILT_WITH_DDK=y
+
+    """)
 
 def _gen_makefile(
         module_symvers_list: list[pathlib.Path],
         output_makefile: pathlib.Path,
 ):
+    # Miscellaneous information
     content = _get_license_str()
+    # If files are re-worked for upstream reasons, this config needs to be
+    # omitted / deleted.
+    content += _get_ddk_marker()
 
     for module_symvers in module_symvers_list:
         content += textwrap.dedent(f"""\
